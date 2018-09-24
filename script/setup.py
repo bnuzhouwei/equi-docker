950084865267e7da5f476e5ce55f87777114c863
root <bnuzhouwei@qq.com>
2018-09-24 17:22:03 +0800

    docker-compose

--- script/setup.py
+++ script/setup.py
@@ -2,8 +2,8 @@ import os
 eqdir = "/usr/equi"
 os.chdir(eqdir)
 def main():
-    print("Install nginx.")
-    os.system("apt-get install nginx")
+    print("Install nginx and docker-compose.")
+    os.system("apt-get install nginx docker-compose")
     print("Build image for web server.")
     os.system("docker build -t web ./image/web")
     print("Build image for database server.")
