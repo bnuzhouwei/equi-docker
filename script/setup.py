b7b401358b5eb52385f4b21967491b6d29544ee6
root <bnuzhouwei@qq.com>
2018-09-24 20:00:24 +0800

    apt install -y

--- script/setup.py
+++ script/setup.py
@@ -3,10 +3,10 @@ eqdir = "/usr/equi"
 os.chdir(eqdir)
 def main():
     print("Install nginx and docker-compose.")
-    os.system("apt-get install nginx docker-compose")
+    os.system("apt-get install -y nginx docker-compose")
     print("Build image for web server.")
-    os.system("docker build -t web ./image/web")
+    os.system("docker build -t web {0}/image/web".format(eqdir))
     print("Build image for database server.")
-    os.system("docker build -t db ./image/db")
+    os.system("docker build -t db {0}/image/db".format(eqdir))
     print("Setup completed successfully.")
 main()
